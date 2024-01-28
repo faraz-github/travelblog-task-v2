e5a970e05414337d98b677a1d42bcda6f622ce76
faraz-github <farazahmad.email@gmail.com>
2024-01-28 15:32:50 +0530

    Add loading and toast notification functionality

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,21 @@
 import "@/styles/globals.css";
+import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { LoadingProvider } from "@/contexts/LoadingContext";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Navbar />
-      <div className="middleContainer">
-        <Component {...pageProps} />
-      </div>
-      <Footer />
+      <LoadingProvider>
+        <Navbar />
+        <div className="middleContainer">
+          <Component {...pageProps} />
+        </div>
+        <Toaster />
+        <Footer />
+      </LoadingProvider>
     </AuthProvider>
   );
 }
